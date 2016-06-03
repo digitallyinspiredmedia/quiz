582c8c4c7f15af0895c1b16a929750ac8cf74149
Kavinraj <sikavinraj@users.noreply.github.com>
2016-06-04 01:04:24 +0530

    Update get-user-profile-picture.php

--- get-user-profile-picture.php
+++ get-user-profile-picture.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
 require_once __DIR__ . '/src/Facebook/autoload.php';
-
 $fb = new Facebook\Facebook([
- 'app_id' => '1276326552379294', // Replace {app-id} with your app id
+  'app_id' => '1276326552379294', // Replace {app-id} with your app id
   'app_secret' => '569e3d4bd889b81743d672446f6bb202',
   'default_graph_version' => 'v2.4',
 ]);
-
 $helper = $fb->getCanvasHelper();
-
-$permissions = ['email']; // optionnal
-
+$permissions = ['user_posts']; // optionnal
 try {
 	if (isset($_SESSION['facebook_access_token'])) {
 	$accessToken = $_SESSION['facebook_access_token'];
@@ -27,25 +23,18 @@ try {
 	echo 'Facebook SDK returned an error: ' . $e->getMessage();
   	exit;
  }
-
 if (isset($accessToken)) {
-
 	if (isset($_SESSION['facebook_access_token'])) {
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	} else {
 		$_SESSION['facebook_access_token'] = (string) $accessToken;
-
 	  	// OAuth 2.0 client handler
 		$oAuth2Client = $fb->getOAuth2Client();
-
 		// Exchanges a short-lived access token for a long-lived one
 		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-
 		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-
 		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
 	}
-
 	// validating the access token
 	try {
 		$request = $fb->get('/me');
@@ -63,13 +52,9 @@ if (isset($accessToken)) {
 		echo 'Facebook SDK returned an error: ' . $e->getMessage();
 		exit;
 	}
-
-	// getting profile picture of the user
+	// getting all posts published by user
 	try {
-		$requestPicture = $fb->get('/me/picture?redirect=false&height=300'); //getting user picture
-		$requestProfile = $fb->get('/me'); // getting basic info
-		$picture = $requestPicture->getGraphUser();
-		$profile = $requestProfile->getGraphUser();
+		$posts_request = $fb->get('/me/posts?limit=500');
 	} catch(Facebook\Exceptions\FacebookResponseException $e) {
 		// When Graph returns an error
 		echo 'Graph returned an error: ' . $e->getMessage();
@@ -79,17 +64,23 @@ if (isset($accessToken)) {
 		echo 'Facebook SDK returned an error: ' . $e->getMessage();
 		exit;
 	}
-	
-	// showing picture on the screen
-	echo "<img src='".$picture['url']."'/>";
-
-	// saving picture
-	$img = __DIR__.'/'.$profile['id'].'.jpg';
-	file_put_contents($img, file_get_contents($picture['url']));
-	
+	$total_posts = array();
+	$posts_response = $posts_request->getGraphEdge();
+	if($fb->next($posts_response)) {
+		$response_array = $posts_response->asArray();
+		$total_posts = array_merge($total_posts, $response_array);
+		while ($posts_response = $fb->next($posts_response)) {	
+			$response_array = $posts_response->asArray();
+			$total_posts = array_merge($total_posts, $response_array);	
+		}
+		print_r($total_posts);
+	} else {
+		$posts_response = $posts_request->getGraphEdge()->asArray();
+		print_r($posts_response);
+	}
   	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
 	$helper = $fb->getRedirectLoginHelper();
-	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/');
+	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
 	echo "<script>window.top.location.href='".$loginUrl."'</script>";
 }
