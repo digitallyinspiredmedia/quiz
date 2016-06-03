29dabda9a3caa095defd0fca8baf5fa6ca9391f4
sikavinraj <sikavinraj@gmail.com>
2016-06-04 00:49:47 +0530

    login call back changed

--- login.php
+++ login.php
@@ -76,6 +76,6 @@ if (isset($accessToken)) {
   	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
 	// replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-	$loginUrl = $helper->getLoginUrl('http://localhost/cavin/v1/login.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('https://cavin.herokuapp.com/login.php', $permissions);
 	echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
