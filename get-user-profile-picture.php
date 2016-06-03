e59a471f9b6eb9d66834e4d7a88dbd16781c71c9
Kavinraj <sikavinraj@users.noreply.github.com>
2016-06-04 01:08:16 +0530

    Update get-user-profile-picture.php

--- get-user-profile-picture.php
+++ get-user-profile-picture.php
@@ -1,86 +1,60 @@
 <?php
-session_start();
+session_start() ;
 require_once __DIR__ . '/src/Facebook/autoload.php';
+
+# login-callback.php
 $fb = new Facebook\Facebook([
-  'app_id' => '1276326552379294', // Replace {app-id} with your app id
-  'app_secret' => '569e3d4bd889b81743d672446f6bb202',
-  'default_graph_version' => 'v2.4',
+  'app_id' => $app_id,
+  'app_secret' => $app_secret,
+  'default_graph_version' => 'v2.2',
 ]);
-$helper = $fb->getCanvasHelper();
-$permissions = ['user_posts']; // optionnal
+
+$helper = $fb->getRedirectLoginHelper();
 try {
-	if (isset($_SESSION['facebook_access_token'])) {
-	$accessToken = $_SESSION['facebook_access_token'];
-	} else {
-  		$accessToken = $helper->getAccessToken();
-	}
+  $accessToken = $helper->getAccessToken();
 } catch(Facebook\Exceptions\FacebookResponseException $e) {
- 	// When Graph returns an error
- 	echo 'Graph returned an error: ' . $e->getMessage();
-  	exit;
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
 } catch(Facebook\Exceptions\FacebookSDKException $e) {
- 	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  	exit;
- }
-if (isset($accessToken)) {
-	if (isset($_SESSION['facebook_access_token'])) {
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	} else {
-		$_SESSION['facebook_access_token'] = (string) $accessToken;
-	  	// OAuth 2.0 client handler
-		$oAuth2Client = $fb->getOAuth2Client();
-		// Exchanges a short-lived access token for a long-lived one
-		$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-		$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-		$fb->setDefaultAccessToken($_SESSION['facebook_access_token']);
-	}
-	// validating the access token
-	try {
-		$request = $fb->get('/me');
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		if ($e->getCode() == 190) {
-			unset($_SESSION['facebook_access_token']);
-			$helper = $fb->getRedirectLoginHelper();
-			$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-			echo "<script>window.top.location.href='".$loginUrl."'</script>";
-			exit;
-		}
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
-	// getting all posts published by user
-	try {
-		$posts_request = $fb->get('/me/posts?limit=500');
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-		// When Graph returns an error
-		echo 'Graph returned an error: ' . $e->getMessage();
-		exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-		// When validation fails or other local issues
-		echo 'Facebook SDK returned an error: ' . $e->getMessage();
-		exit;
-	}
-	$total_posts = array();
-	$posts_response = $posts_request->getGraphEdge();
-	if($fb->next($posts_response)) {
-		$response_array = $posts_response->asArray();
-		$total_posts = array_merge($total_posts, $response_array);
-		while ($posts_response = $fb->next($posts_response)) {	
-			$response_array = $posts_response->asArray();
-			$total_posts = array_merge($total_posts, $response_array);	
-		}
-		print_r($total_posts);
-	} else {
-		$posts_response = $posts_request->getGraphEdge()->asArray();
-		print_r($posts_response);
-	}
-  	// Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
-} else {
-	$helper = $fb->getRedirectLoginHelper();
-	$loginUrl = $helper->getLoginUrl('https://apps.facebook.com/APP_NAMESPACE/', $permissions);
-	echo "<script>window.top.location.href='".$loginUrl."'</script>";
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
 }
+
+if (isset($accessToken)) {
+  // Logged in!
+  $_SESSION['facebook_access_token'] = (string) $accessToken;
+
+  // OAuth 2.0 client handler
+$oAuth2Client = $fb->getOAuth2Client();
+
+// Exchanges a short-lived access token for a long-lived one
+$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
+
+
+// Sets the default fallback access token so we don't have to pass it to each request
+$fb->setDefaultAccessToken($longLivedAccessToken);
+
+try {
+  $response = $fb->get('/me?fields=email'');
+  $userNode = $response->getGraphUser();
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+echo 'Logged in as ' . $userNode->getName();
+
+$graphObject = $response->getGraphObject();
+$email = $graphObject->getProperty('email');  // This is not getting any thing
+
+echo $email; // Empty
+
+}
+?>
