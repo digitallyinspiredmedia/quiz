6df795a9b4ae82b87ae1f3c1fc9121c95b3d8b22
Kavinraj <sikavinraj@users.noreply.github.com>
2016-06-04 01:09:34 +0530

    Update get-user-profile-picture.php

--- get-user-profile-picture.php
+++ get-user-profile-picture.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/src/Facebook/autoload.php';
 
 # login-callback.php
 $fb = new Facebook\Facebook([
-  'app_id' => $app_id,
-  'app_secret' => $app_secret,
+ 'app_id' => '1276326552379294', // Replace {app-id} with your app id
+  'app_secret' => '569e3d4bd889b81743d672446f6bb202',
   'default_graph_version' => 'v2.2',
 ]);
 
